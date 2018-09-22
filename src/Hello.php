523aae1ee040d3574b8defc249183a15030f6992
tiendinh595 <tiendinh595@gmail.com>
2018-09-22 22:54:12 +0700

    init commit

---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vũ Tiến Định
+ * Date: 9/22/18
+ * Time: 10:47 PM
+ * Contact: tiendinh595@gmail.com
+ */
+
+namespace Dinhvt\HelloComposer;
+
+class  Hello
+{
+    function __construct()
+    {
+        print_r(__CLASS__);
+    }
+}
